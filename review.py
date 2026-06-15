443bd61206c4401bfe5ccff128e7a60f6edc967f
KorelskayaElya <101932648+KorelskayaElya@users.noreply.github.com>
2026-06-15 17:05:44 +0300

    Update review.py

--- review.py
+++ review.py
@@ -2,18 +2,16 @@ import random
 
 print("Добро пожаловать в игру 'Угадай число'!")
 print("Попробуй угадать число от 1 до 100.")
-
+user_guess = None
 secret = random.randint(1, 100)  
 attempt = 0 
 
-while True:
+while user_guess != secret:
     user_guess = input("Введите ваше число: ") 
     attempt += 1
-
-    if user_gess == secret:  
-        print("Поздравляем, вы угадали число!")
-        break
-    elif user_guess > secret:  
-        print("Ваше число больше загаданного.")
+    
+    if user_guess > secret:  
+        print("Ваше число больше загаданного. введите число меньше")
     elif user_guess < secret:  
-        print("Ваше число меньше загаданного.")
+        print("Ваше число меньше загаданного. введите число больше")
+print(f"Поздравляем, вы угадали число! Количество попыток {attempt} было использовано")
